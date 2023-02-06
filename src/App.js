49c97e35636cb7777e41f9ecb38b4f0bc132a604
Diabakate Ikary <hegeldirkk@gmail.com>
2023-02-06 17:30:04 +0000

    ok last update

--- src/App.js
+++ src/App.js
@@ -5,43 +5,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // Home Pages Import
-import DigitalAgency from './pages/DigitalAgency';
-import CreativeAgency from './pages/CreativeAgency';
-import PersonalPortfolio from './pages/PersonalPortfolio';
+//import DigitalAgency from './pages/DigitalAgency';
+//import CreativeAgency from './pages/CreativeAgency';
+//import PersonalPortfolio from './pages/PersonalPortfolio';
 import HomeStartup from './pages/HomeStartup';
-import CorporateAgency from './pages/CorporateAgency';
+//import CorporateAgency from './pages/CorporateAgency';
 
 // Blog Import
-import BlogGridView from './pages/BlogGrid';
-import BlogCategory from './pages/Category';
-import BlogArchive from './pages/Archive';
-import BlogDetails from './pages/BlogDetails';
+//import BlogGridView from './pages/BlogGrid';
+//import BlogCategory from './pages/Category';
+//import BlogArchive from './pages/Archive';
+//import BlogDetails from './pages/BlogDetails';
 
 // Service 
-import ServiceOne from './pages/ServiceOne';
-import ServiceTwo from './pages/ServiceTwo';
-import ServiceDetails from './pages/ServiceDetails';
+//import ServiceOne from './pages/ServiceOne';
+//import ServiceTwo from './pages/ServiceTwo';
+//import ServiceDetails from './pages/ServiceDetails';
 
 // Project 
-import ProjectGridOne from './pages/ProjectGridOne';
-import ProjectGridTwo from './pages/ProjectGridTwo';
-import ProjectGridThree from './pages/ProjectGridThree';
-import ProjectGridFive from './pages/ProjectGridFive';
-import ProjectGridFour from './pages/ProjectGridFour';
-import ProjectDetails from './pages/ProjectDetails';
+//import ProjectGridOne from './pages/ProjectGridOne';
+//import ProjectGridTwo from './pages/ProjectGridTwo';
+//import ProjectGridThree from './pages/ProjectGridThree';
+//import ProjectGridFive from './pages/ProjectGridFive';
+//import ProjectGridFour from './pages/ProjectGridFour';
+//import ProjectDetails from './pages/ProjectDetails';
 
 // Pages 
-import Splash from './pages/Splash';
-import AboutUs from './pages/AboutUs';
-import OurOffice from './pages/OurOffice';
-import OurClients from './pages/OurClients';
-import Team from './pages/Team';
-import TeamDetails from './pages/TeamDetails';
-import CaseStudy from './pages/CaseStudy';
-import CaseDetails from './pages/CaseStudyDetails';
-import Testimonials from './pages/Testimonials';
-import PricingTable from './pages/PricingTable';
-import Typography from './pages/Typography';
+//import Splash from './pages/Splash';
+//import AboutUs from './pages/AboutUs';
+//import OurOffice from './pages/OurOffice';
+//import OurClients from './pages/OurClients';
+//import Team from './pages/Team';
+//import TeamDetails from './pages/TeamDetails';
+//import CaseStudy from './pages/CaseStudy';
+//import CaseDetails from './pages/CaseStudyDetails';
+//import Testimonials from './pages/Testimonials';
+//import PricingTable from './pages/PricingTable';
+//import Typography from './pages/Typography';
 import Contact from './pages/Contact';
 import ErrorPage from './pages/404';
 import ComingSoon from './pages/ComingSoon';
